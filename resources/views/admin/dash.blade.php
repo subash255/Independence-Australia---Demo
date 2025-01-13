4221386fb1fafca7b5d40e353c6180b44edef55a
SohanKafle <soka_bca2077@lict.edu.np>
2025-01-13 11:35:03 +0545

    up

--- resources/views/admin/dash.blade.php
+++ resources/views/admin/dash.blade.php
@@ -40,10 +40,10 @@
             </div>
         </div>
 
-        <!-- Income Card -->
+        <!-- Sales Card -->
         <div class="bg-white p-6 rounded-lg text-left hover:shadow-2xl transition-shadow duration-300 flex flex-row items-center justify-between w-full h-20 transform sm:-translate-y-8 lg:-translate-y-12 shadow-lg">
             <div>
-                <h2 class="text-gray-700 font-medium mb-2">Income</h2>
+                <h2 class="text-gray-700 font-medium mb-2">Total Sales</h2>
                 <p class="text-gray-700 font-medium">$400</p>
             </div>
             <div class="bg-purple-600 text-white w-12 h-12 flex items-center justify-center rounded-full">
@@ -59,17 +59,6 @@
             </div>
             <div class="bg-green-500 text-white w-12 h-12 flex items-center justify-center rounded-full">
                 <i class="ri-shopping-cart-fill text-2xl"></i>
-            </div>
-        </div>
-
-        <!-- Revenue Card -->
-        <div class="bg-white p-6 rounded-lg text-left hover:shadow-2xl transition-shadow duration-300 flex flex-row items-center justify-between w-full h-20 transform sm:-translate-y-4 lg:-translate-y-8 shadow-lg">
-            <div>
-                <h2 class="text-gray-700 font-medium mb-2">Revenue</h2>
-                <p class="text-gray-700 font-medium">100</p>
-            </div>
-            <div class="bg-blue-500 text-white w-12 h-12 flex items-center justify-center rounded-full">
-                <i class="ri-money-dollar-circle-fill text-2xl"></i>
             </div>
         </div>
 
